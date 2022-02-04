3bb9b852298aaec78dc7e44ad359cd5a64771799
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-02-04 10:13:41 -0300

    exercicios

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -0,0 +1,70 @@
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+//for (let index = 0; index < numbers.length; index += 1) {
+  //  console.log(numbers[index])}
+
+//Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+//let soma = 0
+
+//for (let index = 0; index < numbers.length; index += 1) {
+//soma = soma + numbers[index]}
+//console.log(soma)
+
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+//A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+
+//let soma = 0
+
+//for (let index = 0; index < numbers.length; index += 1) {
+//soma = soma + numbers[index]}
+
+//let mediaAritimetica = soma / numbers.length
+
+//console.log(mediaAritimetica)
+
+//Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+//if(mediaAritimetica>20){
+  //console.log("valor maior que 20")
+//} else {console.log("valor menor ou igual a 20")}
+
+//Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+//let maiorValor = 0
+
+//for (let index = 0; index < numbers.length; index += 1){
+  //if (numbers[index]>maiorValor){
+    //maiorValor = numbers[index]}}
+//console.log(maiorValor)
+
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+//let numerosImpares = []
+
+//for (let index = 0; index < numbers.length; index += 1){
+  //if(numbers[index]%2!=0){
+//numerosImpares.push(numbers[index])}}
+
+  //if (numerosImpares.length>0){console.log(numerosImpares)}
+  //else {console.log("nenhum valor ímpar encontrado")}
+
+  //Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+  //let menorNumero = numbers[0]
+  //for (let index = 0; index < numbers.length; index += 1)
+//{if (numbers[index]<menorNumero){
+  //menorNumero = numbers[index]
+//}
+//}
+//console.log(menorNumero)
+  
+//let numbers = []  
+
+//for (let contador=1; contador<=25; contador += 1)
+//{numbers.push(contador)}
+
+//console.log(numbers)
+
+//for (let index = 0; index < numbers.length; index+=1) {
+//console.log(numbers[index]/2)}
+
