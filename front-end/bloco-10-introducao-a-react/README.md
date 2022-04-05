5149b9aac2fa94ebdc824ff6847044e30fb351ba
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-04-05 19:41:15 -0300

    adicionando exercícios e modificando readme

---
+++ front-end/bloco-10-introducao-a-react/README.md
@@ -0,0 +1,3 @@
+## Bloco 10 - Introdução à React
+
+Neste bloco foi mostrado os primeiros passos em React, como utliziar JSX e os componentes.
---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Pokemon from './Pokemon';
+import Pokedex from './Pokedex';
+
+function App() {
+  return (
+    <div>
+    <header className='App-header'>Pokedex</header>
+    <Pokedex />
+    </div>
+
+  );
+}
+
+export default App;
